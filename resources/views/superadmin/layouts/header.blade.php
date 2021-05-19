9d1e2681a1867a376dc14709616dc994900afdc7
William Moraes <william.morais@intergalaxy.io>
2021-05-19 11:27:31 -0300

    Updates

--- resources/views/superadmin/layouts/header.blade.php
+++ resources/views/superadmin/layouts/header.blade.php
@@ -1,154 +1,25 @@
-<header class="top-header">
-    <nav class="navbar navbar-expand">
-        <div class="left-topbar d-flex align-items-center">
-            <a href="javascript:;" class="toggle-btn">	<i class="bx bx-menu"></i>
+<nav class="navbar navbar-expand-lg main-navbar">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="/">
+        <img class="header-logo" src="{{ asset('assets/admin-theme/images/global/header-logo.svg') }}">
+    </a>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+            <a class="nav-link" href="/"></a>
+        </li>
+        </ul>
+        <div class="">
+        <div>
+            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="header-exit-link">
+            <img src="{{ asset('assets/admin-theme/images/global/exit.svg') }}"> Sair
             </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+            </form>
         </div>
-
-        <div class="right-topbar ml-auto">
-            <ul class="navbar-nav">
-                <li class="nav-item search-btn-mobile">
-                    <a class="nav-link position-relative" href="javascript:;">	<i class="bx bx-search vertical-align-middle"></i>
-                    </a>
-                </li>
-                <li class="nav-item dropdown dropdown-lg">
-                    <a class="nav-link dropdown-toggle dropdown-toggle-nocaret position-relative" href="javascript:;" data-toggle="dropdown">	<i class="bx bx-bell vertical-align-middle"></i>
-                        <span class="msg-count">8</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a href="javascript:;">
-                            <div class="msg-header">
-                                <h6 class="msg-header-title">8 New</h6>
-                                <p class="msg-header-subtitle">Application Notifications</p>
-                            </div>
-                        </a>
-                        <div class="header-notifications-list">
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="media align-items-center">
-                                    <div class="notify bg-light-primary text-primary"><i class="bx bx-group"></i>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="msg-name">New Customers<span class="msg-time float-right">14 Sec
-                                            ago</span></h6>
-                                        <p class="msg-info">5 new user registered</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="media align-items-center">
-                                    <div class="notify bg-light-danger text-danger"><i class="bx bx-cart-alt"></i>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="msg-name">New Orders <span class="msg-time float-right">2 min
-                                            ago</span></h6>
-                                        <p class="msg-info">You have recived new orders</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="media align-items-center">
-                                    <div class="notify bg-light-shineblue text-shineblue"><i class="bx bx-file"></i>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="msg-name">24 PDF File<span class="msg-time float-right">19 min
-                                            ago</span></h6>
-                                        <p class="msg-info">The pdf files generated</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="media align-items-center">
-                                    <div class="notify bg-light-cyne text-cyne"><i class="bx bx-send"></i>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="msg-name">Time Response <span class="msg-time float-right">28 min
-                                            ago</span></h6>
-                                        <p class="msg-info">5.1 min avarage time response</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="media align-items-center">
-                                    <div class="notify bg-light-purple text-purple"><i class="bx bx-home-circle"></i>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="msg-name">New Product Approved <span
-                                            class="msg-time float-right">2 hrs ago</span></h6>
-                                        <p class="msg-info">Your new product has approved</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="media align-items-center">
-                                    <div class="notify bg-light-warning text-warning"><i class="bx bx-message-detail"></i>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="msg-name">New Comments <span class="msg-time float-right">4 hrs
-                                            ago</span></h6>
-                                        <p class="msg-info">New customer comments recived</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="media align-items-center">
-                                    <div class="notify bg-light-success text-success"><i class='bx bx-check-square'></i>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="msg-name">Your item is shipped <span class="msg-time float-right">5 hrs
-                                            ago</span></h6>
-                                        <p class="msg-info">Successfully shipped your item</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="media align-items-center">
-                                    <div class="notify bg-light-sinata text-sinata"><i class='bx bx-user-pin'></i>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="msg-name">New 24 authors<span class="msg-time float-right">1 day
-                                            ago</span></h6>
-                                        <p class="msg-info">24 new authors joined last week</p>
-                                    </div>
-                                </div>
-                            </a>
-                            <a class="dropdown-item" href="javascript:;">
-                                <div class="media align-items-center">
-                                    <div class="notify bg-light-mehandi text-mehandi"><i class='bx bx-door-open'></i>
-                                    </div>
-                                    <div class="media-body">
-                                        <h6 class="msg-name">Defense Alerts <span class="msg-time float-right">2 weeks
-                                            ago</span></h6>
-                                        <p class="msg-info">45% less alerts last 4 weeks</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <a href="javascript:;">
-                            <div class="text-center msg-footer">View All Notifications</div>
-                        </a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown dropdown-user-profile">
-                    <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="javascript:;" data-toggle="dropdown">
-                        <div class="media user-box align-items-center">
-                            <div class="media-body user-info">
-                                <p class="user-name mb-0">{{Auth::user()->name}}</p>
-                            </div>
-                            <img src="https://via.placeholder.com/110x110" class="user-img" alt="user avatar">
-                        </div>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item">
-                                <div class="icon-base">	<i class="fadeIn animated bx bx-log-out"></i>
-                                    <span style="display: inline;">Logout</span>
-                                </div>
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                        </form>
-                    </div>
-                </li>
-            </ul>
         </div>
-    </nav>
-</header>
+    </div>
+</nav>
