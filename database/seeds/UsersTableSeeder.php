30897d3c3095307997d6c8b2e85463683aa8897f
William Moraes <william.morais@intergalaxy.io>
2021-05-21 13:21:03 -0300

    Alter roles

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,7 +14,8 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $roleSuperAdmin = Defender::createRole('superadmin');
-        $roleAdmin = Defender::createRole('admin');
+        $roleManager = Defender::createRole('manager');
+        $roleArm = Defender::createRole('arm');
         $roleUser = Defender::createRole('user');
         $roleRestaurantUser = Defender::createRole('restaurantuser');
 
@@ -27,20 +28,29 @@ class UsersTableSeeder extends Seeder
             'email_token' => 'superadmintoken'
         ]);
 
-        $admin = App\User::create([
-            'name' => 'Admin',
-            'email' => 'admin@meualmoco.dev',
-            'password' => Hash::make('admin'),
+        $manager = App\User::create([
+            'name' => 'Manager',
+            'email' => 'manager@meualmoco.dev',
+            'password' => Hash::make('manager'),
             'cpf' => '22222222222',
             'billing_code' => 'CCC6776DD',
-            'email_token' => 'admintoken'
+            'email_token' => 'managertoken'
+        ]);
+
+        $arm = App\User::create([
+            'name' => 'Arm',
+            'email' => 'arm@meualmoco.dev',
+            'password' => Hash::make('arm'),
+            'cpf' => '33333333333',
+            'billing_code' => 'BCC6776DD',
+            'email_token' => 'armtoken'
         ]);
 
         $user = App\User::create([
             'name' => 'User',
             'email' => 'user@meualmoco.dev',
             'password' => Hash::make('user'),
-            'cpf' => '33333333333',
+            'cpf' => '44444444444',
             'billing_code' => 'MKC277655',
             'email_token' => 'usertoken'
         ]);
@@ -49,13 +59,14 @@ class UsersTableSeeder extends Seeder
             'name' => 'Restaurant User',
             'email' => 'restaurantuser@meualmoco.dev',
             'password' => Hash::make('user'),
-            'cpf' => '44444444444',
+            'cpf' => '55555555555',
             'billing_code' => 'TYC2C46P0',
             'email_token' => 'restaurantusertoken'
         ]);
 
-        $admin->attachRole($roleAdmin);
         $superAdminadmin->attachRole($roleSuperAdmin);
+        $manager->attachRole($roleManager);
+        $arm->attachRole($roleArm);
         $user->attachRole($roleUser);
         $restaurantUser->attachRole($roleRestaurantUser);
     }
