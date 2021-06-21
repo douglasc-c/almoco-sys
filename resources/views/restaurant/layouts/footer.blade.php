88d36fa5d9f7ef517666a9f9ef87852185b42c75
Matheus Gabardo Messias <matheus.gabardo@intergalaxy.io>
2021-06-21 10:20:16 -0300

    remove user footer

--- resources/views/restaurant/layouts/footer.blade.php
+++ resources/views/restaurant/layouts/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="footer main-footer">
+<!-- <footer class="footer main-footer">
     <div class="container">
         <span class="footer-text">2021 © MEUALMOÇO</span>
     </div>
-</footer>
+</footer> -->
