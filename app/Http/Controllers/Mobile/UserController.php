9caabfae6dd641de8d6d35c71840fdb02b7604c1
William Moraes <william.morais@intergalaxy.io>
2021-08-23 13:55:20 -0300

    fix

--- app/Http/Controllers/Mobile/UserController.php
+++ app/Http/Controllers/Mobile/UserController.php
@@ -27,6 +27,7 @@ class UserController extends Controller
                 if ($user->first_access == 1 || $user->first_access == true) {
                     $user->update([
                         'password' => bcrypt($request->password),
+                        'name' => $request->name,
                         'cpf' => $request->cpf,
                         'billing_code' => $request->billing_code,
                         'first_access' => 0,
