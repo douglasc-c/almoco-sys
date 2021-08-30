d686882f977adff6991d6a0804cbc48c6ad35c37
Lucas Krul <lucas.krul@intergalaxy.io>
2021-08-30 15:09:54 -0300

    Fix

--- app/Http/Controllers/Mobile/UserController.php
+++ app/Http/Controllers/Mobile/UserController.php
@@ -10,54 +10,33 @@ class UserController extends Controller
 {
     public function update(Request $request)
     {
-        $client = new \GuzzleHttp\Client();
-        $result = $client->post('http://67.207.95.144/api/mobile/v1/checkCode', [
-            'json' => [
-                'document_number' => $request->cpf,
-                'billing_code' => $request->billing_code,
-            ]
-        ]);
+        $user = auth('api')->user();
 
-        $res = json_decode($result->getBody()->getContents());
-
-        if (isset($res)) {
-            if ($res->status) {
-                $user = auth('api')->user();
-
-                if ($user->first_access == 1 || $user->first_access == true) {
-                    $user->update([
-                        'password' => bcrypt($request->password),
-                        'cpf' => $request->cpf,
-                        'billing_code' => $request->billing_code,
-                        'first_access' => 0,
-                    ]);
-                } else {
-                    if (!Hash::check($request->password, $user->password)) {
-                        return response()->json([
-                            "error" => "Ocorreu um erro", 
-                            "message" => 'Senha atual não confere',
-                            'pass' => $request->password,
-                            'u_pass' => $user->password,
-                            'hash' => Hash::check($request->password, $user->password)
-                        ], 400);
-                    }
-
-                    $user->update([
-                        'password' => bcrypt($request->newPassword),
-                    ]);
-                }
-
-                $user = auth('api')->user();
-
-                $user->role_id = $user->roles->first()->id;
-
-                return response()->json([ 'user' => $user ]);
-            } else {
-                return response()->json(["error" => "Ops, algo de errado", "message" => "Código da conta e/ou CPF incorretos"], 401);
-            }
+        if ($user->first_access == 1 || $user->first_access == true) {
+            $user->update([
+                'password' => bcrypt($request->password),
+                'cpf' => $request->cpf,
+                'billing_code' => '-',
+                'first_access' => 0,
+            ]);
         } else {
-            return response()->json(["error" => "Ops, algo de errado", "message" => "Código da conta e/ou CPF incorretos"], 401);
+            if (!Hash::check($request->password, $user->password)) {
+                return response()->json([
+                    "error" => "Ocorreu um erro", 
+                    "message" => 'Senha atual não confere',
+                ], 400);
+            }
+
+            $user->update([
+                'password' => bcrypt($request->newPassword),
+            ]);
         }
+
+        $user = auth('api')->user();
+
+        $user->role_id = $user->roles->first()->id;
+
+        return response()->json([ 'user' => $user ]);
     }
 
     public function changeToken(Request $request) {
